34282608cc2d17bf8cdbe2ac2a1a2c6a777d68ae
Richa <richa.parekh@pavansgroup.in>
2025-03-26 19:14:57 +0530

    Acme Rockets UI Clone with Tailwind

---
+++ acme-rockets-clone/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./build/*.html", // Watch only necessary files
+	],
+	theme: {
+		extend: {}, // Extend Tailwind as needed
+	},
+	plugins: [],
+};
