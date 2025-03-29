a622c1b6425d434bcf59ea5c23793ef4528cecd5
Richa <richa.parekh@pavansgroup.in>
2025-03-29 19:26:43 +0530

    Acme Rockets Clone with Tailwind

--- acme-rockets-clone/tailwind.config.js
+++ acme-rockets-clone/tailwind.config.js
@@ -2,9 +2,16 @@
 export default {
 	content: [
 		"./build/*.html", // Watch only necessary files
+		"./src/**/*.css", // Ensure Tailwind sees your input.css
 	],
 	theme: {
-		extend: {}, // Extend Tailwind as needed
+		extend: {
+			'screens': {
+				'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+				'tallscreen': { 'raw': '(min-aspect-ratio: 1/2)' },
+			}
+
+		},
 	},
 	plugins: [],
 };
