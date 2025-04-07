a112878bec3c52ccb2c00fe04f33860edc3d6b31
Richa <richa.parekh@pavansgroup.in>
2025-04-07 19:34:30 +0530

    Google UI - Neobrutalism Theme

--- google-ui-neobrutalism/build/js/main.js
+++ google-ui-neobrutalism/build/js/main.js
@@ -1,14 +1,25 @@
 const initApp = () => {
-    const searchBtn = document.getElementById('search-btn')
-    const searchModal = document.getElementById('search-modal')
-    const closeBtn = document.getElementById('close-btn')
-    const toggleMenu = () => {
-        searchModal.classList.toggle('hidden')
-        searchModal.classList.toggle('flex')
-    }
+	const searchBtn = document.getElementById("search-btn");
+	const searchModal = document.getElementById("search-modal");
+	const closeBtn = document.getElementById("close-btn");
+	
+	const toggleMenu = () => {
+		searchModal.classList.toggle("hidden");
+		searchModal.classList.toggle("flex");
+		document.getElementById('search').value = "";
+	};
 
-    searchBtn.addEventListener('click', toggleMenu)
-    closeBtn.addEventListener('click', toggleMenu)
-}
+	searchBtn.addEventListener("click", toggleMenu);
+	closeBtn.addEventListener("click", toggleMenu);
+};
 
-document.addEventListener('DOMContentLoaded', initApp)
+const changeMode = () => {
+	const element = document.getElementById("root");
+	if (element.classList.contains("dark")) {
+		element.classList.remove("dark");
+	} else {
+		element.classList.add("dark");
+	}
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
